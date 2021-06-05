ca4723d2c69b983fd0a97b07af5e5872bbc70ef5
jun-shinkai <shinjyuna@gmail.com>
2021-06-05 10:55:49 +0900

    本人情報確認

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,23 +37,37 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include "Password には英字と数字の両方を含めて設定してください"
     end
     it 'パスワードとパスワード（確認）は、値の一致が必須であること' do
-      user = build(:user, encrypted_password: nil)
-      user.valid?
-      binding.pry
-      expect(user.errors[:encrypted_password]).to include("を入力してください")
+      @user.password_confirmation = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
   end
 
   describe '本人情報確認' do
-    it 'お名前(全角)は、名字と名前がそれぞれ必須であること。'
+    it 'お名前(全角)は、名字と名前がそれぞれ必須であること。'do
+      @user.last_name = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Last name can't be blank"
     end
-    it 'お名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が必須である'
+    it 'お名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が必須である' do
+      @user.last_name ='444444'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Nickname is invalid. Input full-width characters."
     end
-    it 'お名前カナ(全角)は、名字と名前がそれぞれ必須であること。'
+    it 'お名前カナ(全角)は、名字と名前がそれぞれ必須であること。' do
+      @user.last_name_kana  = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Last name kana can't be blank"
     end
-    it 'お名前カナ(全角)は、全角（カタカナ）での入力が必須であること。'
+    it 'お名前カナ(全角)は、全角（カタカナ）での入力が必須であること。'do
+    @user.last_name_kana = "5464"
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Nickname is invalid. Input full-width characters."
     end
-    it '生年月日が必須であること。'
+    it '生年月日が必須であること。'do
+      @user.birth_date = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Birth date can't be blank"
     end
 
   end  
